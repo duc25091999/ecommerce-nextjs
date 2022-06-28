91740db77c2850d43cec67add3ac23fe5c4c7266
duc25091999 <duc25091999@gmail.com>
2022-06-28 21:05:02 +0700

    add docker

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-    env: {
-        "BASE_URL": "https://nike-store-nextjs.herokuapp.com/",
-        "MONGODB_URL": "mongodb+srv://duc25091999:duc25091999@cluster0.6wscb.mongodb.net/?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "secret_token",
-        "REFRESH_TOKEN_SECRET": "refresh_token",
-        "PAYPAL_CLIENT_ID": "AXLVePMkMzso6g_iYQLlUj9_c-ksUFXKnQ7cIjcJTJ6B7E9C_zxztehLFTO84WATHgvbesM2F7QH0Wqg",
-        "CLOUD_UPDATE_PRESET": "h1e1usnf",
-        "CLOUD_NAME": "dypq5slsf",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/dypq5slsf/image/upload"
-    }
-}
+  env: {
+    BASE_URL: "https://nike-store-nextjs.herokuapp.com/",
+    MONGODB_URL:
+      "mongodb+srv://duc25091999:duc25091999@cluster0.6wscb.mongodb.net/?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "secret_token",
+    REFRESH_TOKEN_SECRET: "refresh_token",
+    PAYPAL_CLIENT_ID:
+      "AXLVePMkMzso6g_iYQLlUj9_c-ksUFXKnQ7cIjcJTJ6B7E9C_zxztehLFTO84WATHgvbesM2F7QH0Wqg",
+    CLOUD_UPDATE_PRESET: "h1e1usnf",
+    CLOUD_NAME: "dypq5slsf",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/dypq5slsf/image/upload",
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+};
